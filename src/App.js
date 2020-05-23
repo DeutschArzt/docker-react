4e69bc5dcb3b6b770092b12fba40024405ad7c0f
Matthew Buhr <mjbuhr@Matthews-MacBook-Pro.local>
2020-05-23 02:26:46 -0500

    Changed app text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" /><p>
+          Fur alles seit.
+        </p><p>
+          Fur alles seit.
+        </p><p>
+          Fur alles seit.
+        </p><p>
+          Fur alles seit.
+        </p><p>
+          Fur alles seit.
+        </p><p>
+          Fur alles seit.
+        </p><p>
+          Fur alles seit.
+        </p>
         <p>
           Fur alles seit.
         </p>
