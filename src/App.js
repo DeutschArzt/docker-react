da724e2d8761e4813eeb596dbbc4c015e9902624
Matthew Buhr <mjbuhr@Matthews-MacBook-Pro.local>
2020-05-22 22:50:29 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Fur alles seit.
+        </p>
+        <p>
+          Fur alles seit.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
